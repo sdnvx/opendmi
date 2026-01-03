ea82a39da40ceb554b847fa36494745df032e0f2
xerrial <dimass1129@gmail.com>
2026-01-03 15:15:24 +1000

    System event log decoding draft #53

--- libopendmi/include/opendmi/entity/system-event-log.h
+++ libopendmi/include/opendmi/entity/system-event-log.h
@@ -11,9 +11,144 @@
 
 #include <opendmi/entity.h>
 
+typedef struct dmi_system_event_log      dmi_system_event_log_t;
+typedef struct dmi_system_event_log_data dmi_system_event_log_data_t;
+typedef union  dmi_system_log_status     dmi_system_log_status_t;
+
+typedef enum dmi_system_log_access_method {
+    DMI_SYSTEM_LOG_ACCESS_METHOD_INDEXED_IO_8BIT_1_1 = 0x00, // Indexed I/O: 1 8-bit index port, 1 8-bit data port
+    DMI_SYSTEM_LOG_ACCESS_METHOD_INDEXED_IO_8BIT_2_1 = 0x01, // Indexed I/O: 2 8-bit index ports, 1 8-bit data port
+    DMI_SYSTEM_LOG_ACCESS_METHOD_INDEXED_IO_16BIT    = 0x02, // Indexed I/O: 1 16-bit index port, 1 8-bit data port
+    DMI_SYSTEM_LOG_ACCESS_METHOD_MMAP_32BIT_ADDR     = 0x03, // Memory-mapped physical 32-bit address
+    DMI_SYSTEM_LOG_ACCESS_METHOD_DATA_FUNCTIONS      = 0x04, // Available through general-purpose non-volatile data functions
+    // Reserved: 0x05 .. 0x7H
+    DMI_SYSTEM_LOG_ACCESS_METHOD_OEM_START           = 0x80, // 0x80 .. 0xFF Firmware vendor/OEM-specific
+    DMI_SYSTEM_LOG_ACCESS_METHOD_OEM_END             = 0xFF, // 0x80 .. 0xFF Firmware vendor/OEM-specific
+} dmi_system_log_access_method_t;
+
+typedef enum dmi_system_log_header_fmt {
+    DMI_SYSTEM_LOG_HEADER_FMT_NO_HEADER = 0x00, // No header
+    DMI_SYSTEM_LOG_HEADER_FMT_TYPE_1    = 0x01, // Type 1 log header
+    // Reserved: 0x02 .. 0x7EH
+    DMI_SYSTEM_LOG_HEADER_FMT_OEM_START = 0x80, // 0x80 .. 0xFF Firmware OEM-specific format
+    DMI_SYSTEM_LOG_HEADER_FMT_OEM_END   = 0xFF, // 0x80 .. 0xFF Firmware OEM-specific format
+} dmi_system_log_header_fmt_t;
+
+dmi_packed_union(dmi_system_log_status)
+{
+    dmi_byte_t __value;
+
+    dmi_packed_struct()
+    {
+        bool is_log_area_valid : 1;
+        bool is_log_area_full  : 1;
+
+        uint8_t __reserved : 6;
+    };
+};
+
+dmi_packed_union(dmi_system_log_access_method_addr)
+{
+    dmi_dword_t __value;
+
+    dmi_packed_struct()
+    {
+        uint16_t index_addr;
+        uint16_t data_addr;
+    } io;
+
+    uint32_t phys_addr_32bit;
+
+    uint16_t gpnv_handle;
+};
+
+/**
+ * @brief System event log structure (type 15).
+ */
+dmi_packed_struct(dmi_system_event_log_data)
+{
+    /**
+     * @brief SMBIOS structure header.
+     */
+    dmi_header_t header;
+
+    /**
+     * @brief Log area length.
+     */
+    dmi_word_t length;
+
+    /**
+     * @brief Log header start offset.
+     */
+    dmi_word_t header_offset;
+
+    /**
+     * @brief Log data start offset.
+     */
+    dmi_word_t data_offset;
+
+    /**
+     * @brief Access method.
+     */
+    dmi_byte_t access_method;
+
+    /**
+     * @brief Log status.
+     */
+    dmi_byte_t status;
+
+    /**
+     * @brief Log change token.
+     */
+    dmi_dword_t change_token;
+
+    /**
+     * @brief Access method address.
+     */
+    dmi_dword_t access_method_addr;
+
+    /**
+     * @brief Log header format.
+     */
+    dmi_byte_t format;
+
+    /**
+     * @brief Number of supported log type descriptors.
+     */
+    dmi_byte_t type_descr_count;
+
+    /**
+     * @brief Length of each log type.
+     */
+    dmi_byte_t type_descr_length;
+
+    /**
+     * @brief List of supported event log type descriptors.
+     */
+    dmi_byte_t type_descr_list[];
+};
+
+struct dmi_system_event_log
+{
+    dmi_system_log_access_method_t access_method;
+
+    dmi_system_log_status_t status;
+};
+
 /**
  * @brief System event log entity specification.
  */
 extern const dmi_entity_spec_t dmi_system_event_log_spec;
+
+/**
+ * @internal
+ */
+dmi_system_event_log_t *
+dmi_system_event_log_decode(const dmi_entity_t *entity, dmi_version_t *plevel);
+
+/**
+ * @internal
+ */
+void dmi_system_event_log_free(dmi_system_event_log_t *info);
 
 #endif // !OPENDMI_ENTITY_SYSTEM_EVENT_LOG_H
--- libopendmi/src/entity/system-event-log.c
+++ libopendmi/src/entity/system-event-log.c
@@ -5,9 +5,71 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
 #include <opendmi/entity/system-event-log.h>
+#include <opendmi/utils.h>
+
+static const dmi_name_set_t dmi_system_log_access_method_names =
+{
+    .code  = "access-methods",
+    .names = {
+        {
+            .id   = DMI_SYSTEM_LOG_ACCESS_METHOD_INDEXED_IO_8BIT_1_1,
+            .code = "indexed-io-8bit-1-1",
+            .name = "Indexed I/O: 1 8-bit index port, 1 8-bit data port"
+        },
+        {
+            .id   = DMI_SYSTEM_LOG_ACCESS_METHOD_INDEXED_IO_8BIT_2_1,
+            .code = "indexed-io-8bit-2-1",
+            .name = "Indexed I/O: 2 8-bit index ports, 1 8-bit data port"
+        },
+        {
+            .id   = DMI_SYSTEM_LOG_ACCESS_METHOD_INDEXED_IO_16BIT,
+            .code = "indexed-io-16bit",
+            .name = "Indexed I/O: 1 16-bit index port, 1 8-bit data port"
+        },
+        {
+            .id   = DMI_SYSTEM_LOG_ACCESS_METHOD_MMAP_32BIT_ADDR,
+            .code = "memory-mapped-32bit-address",
+            .name = "Memory-mapped physical 32-bit address"
+        },
+        {
+            .id   = DMI_SYSTEM_LOG_ACCESS_METHOD_DATA_FUNCTIONS,
+            .code = "data-functions",
+            .name = "General-purpose non-volatile data functions"
+        },
+        DMI_NAME_NULL
+    }
+};
+
+static const dmi_name_set_t dmi_system_log_status_names =
+{
+    .code = "status",
+    .names = {
+        {
+            .id   = 0,
+            .code = "log-area-valid",
+            .name = "Log area valid"
+        },
+        {
+            .id   = 1,
+            .code = "log-area-full",
+            .name = "Log area full"
+        },
+        DMI_NAME_NULL
+    }
+};
 
 const dmi_attribute_t dmi_system_event_log_attrs[] =
 {
+    DMI_ATTRIBUTE(dmi_system_event_log_t, access_method, ENUM, {
+        .code   = "access-method",
+        .name   = "Access Method",
+        .values = &dmi_system_log_access_method_names
+    }),
+    DMI_ATTRIBUTE(dmi_system_event_log_t, status, SET, {
+        .code = "status",
+        .name = "Status",
+        .values = &dmi_system_log_status_names,
+    }),
     DMI_ATTRIBUTE_NULL
 };
 
@@ -18,5 +80,42 @@ const dmi_entity_spec_t dmi_system_event_log_spec =
     .type        = DMI_TYPE_SYSTEM_EVENT_LOG,
     .min_version = DMI_VERSION(2, 0, 0),
     .min_length  = 0x14,
-    .attributes  = dmi_system_event_log_attrs
+    .attributes  = dmi_system_event_log_attrs,
+    .handlers    = {
+        .decode = (dmi_entity_decode_fn_t)dmi_system_event_log_decode,
+        .free   = (dmi_entity_free_fn_t)dmi_system_event_log_free
+    }
 };
+
+dmi_system_event_log_t *
+dmi_system_event_log_decode(const dmi_entity_t *entity, dmi_version_t *plevel)
+{
+    dmi_system_event_log_t *info;
+    dmi_version_t level = dmi_version(2, 0, 0);
+    const dmi_system_event_log_data_t *data;
+
+    data = dmi_cast(data, dmi_entity_data(entity, DMI_TYPE_SYSTEM_EVENT_LOG));
+    if (data == nullptr)
+        return nullptr;
+
+    info = dmi_alloc(entity->context, sizeof(*info));
+    if (info == nullptr)
+        return nullptr;
+
+    info->access_method  = dmi_value(data->access_method);
+    info->status.__value = dmi_value(data->status);
+
+    if (entity->body_length >= 0x14) {
+        level = dmi_version(2, 1, 0);
+    }
+
+    if (plevel != nullptr)
+        *plevel = level;
+
+    return info;
+}
+
+void dmi_system_event_log_free(dmi_system_event_log_t *info)
+{
+    dmi_free(info);
+}
